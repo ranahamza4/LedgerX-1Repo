da1eed6f0fa574af5cad5ab3bf0c2df410b162c0
Sarmad-Sohaib <sarmadshiab.00@gmail.com>
2020-11-06 20:48:42 +0500

    This is 1st commit with firebase integration

---
+++ app/src/main/java/com/bigcompany/ledgerx/MainActivity.java
@@ -0,0 +1,18 @@
+package com.bigcompany.ledgerx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.FirebaseApp;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}

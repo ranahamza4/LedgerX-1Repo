aab117a63146af856f27cc990b0a3adab1939536
Sarmad-Sohaib <sarmadshiab.00@gmail.com>
2020-11-07 17:18:36 +0500

    Login and SignUp design

--- app/src/main/java/com/bigcompany/ledgerx/MainActivity.java
+++ app/src/main/java/com/bigcompany/ledgerx/MainActivity.java
@@ -1,16 +1,27 @@
 package com.bigcompany.ledgerx;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 public class MainActivity extends AppCompatActivity {
 
-
+    TextView mSignUpTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.getSupportActionBar().hide();
 
+        mSignUpTxt = findViewById(R.id.signUp_textView);
+
+        mSignUpTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignupActivity.class));
+            }
+        });
     }
 }

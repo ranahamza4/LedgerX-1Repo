e725918efc31179276fe490726200f95d66e5d25
Sarmad-Sohaib <sarmadshiab.00@gmail.com>
2020-11-08 12:46:38 +0500

    Action menu and search components added in Home activity.

---
+++ app/src/main/java/com/bigcompany/ledgerx/HomeActivity.java
@@ -0,0 +1,30 @@
+package com.bigcompany.ledgerx;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class HomeActivity extends AppCompatActivity {
+    ActionBar actionBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("Memory of ------");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}

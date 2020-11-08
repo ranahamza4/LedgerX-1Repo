f1fb272874ad3dec62d7dde8813487e53f3647ad
ranahamza4 <rdxat47@gmail.com>
2020-11-09 00:07:54 +0500

    HomeActivity Recycler Work Done

--- app/src/main/java/com/bigcompany/ledgerx/HomeActivity.java
+++ app/src/main/java/com/bigcompany/ledgerx/HomeActivity.java
@@ -1,11 +1,13 @@
 package com.bigcompany.ledgerx;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class HomeActivity extends AppCompatActivity {
     ActionBar actionBar;
@@ -17,6 +19,30 @@ public class HomeActivity extends AppCompatActivity {
         actionBar = getSupportActionBar();
         actionBar.setTitle("Memory of ------");
         /* ------ in line 18 will be username of signed in user as per*/
+
+  /*---UI Related Code*/
+        RecyclerView mrecyclerView =findViewById(R.id.homeRecyclerView);
+        mrecyclerView.setHasFixedSize(true);
+        mrecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        //Code Just For Testing purpose. Here real data of users will be applied afterwards
+        UserData[] myUserData=new UserData[]{
+                new UserData("Rana Hamza","@RD47","rdhamza47",500,1000),
+                new UserData("Sarmad Sohaib","@RD47","rdhamza47",900,100),
+                new UserData("Khaleek Ahmad","@RD47","rdhamza47",5010,3400),
+                new UserData("Hamood Zicco","@RD47","rdhamza47",5120,1340),
+                new UserData("Muhammad Nouman","@RD47","rdhamza47",40,10),
+                new UserData("Haseeb Rao","@RD47","rdhamza47",300,1200),
+                new UserData("Rehan Anjum","@RD47","rdhamza47",900,1500),
+                new UserData("Aman Usman","@RD47","rdhamza47",1500,300),
+                new UserData("Jutt da Muqabla","@RD47","rdhamza47",400,90000),
+
+        };
+        UserDataAdapter userDataAdapter= new UserDataAdapter(myUserData,this);
+        mrecyclerView.setAdapter(userDataAdapter);
+
+
     }
 
     @Override
--- app/src/main/java/com/bigcompany/ledgerx/MainActivity.java
+++ app/src/main/java/com/bigcompany/ledgerx/MainActivity.java
@@ -3,12 +3,14 @@ package com.bigcompany.ledgerx;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     TextView mSignUpTxt;
+    Button mLogInBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,11 +18,21 @@ public class MainActivity extends AppCompatActivity {
         this.getSupportActionBar().hide();
 
         mSignUpTxt = findViewById(R.id.signUp_textView);
+        mLogInBtn=(Button)findViewById(R.id.login_button);
 
         mSignUpTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, SignupActivity.class));
+            }
+        });
+
+
+
+        mLogInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,HomeActivity.class));
             }
         });
     }

d49b1c3123cd75483a4f08011ec445012fa249b2
Sarmad-Sohaib <sarmadshiab.00@gmail.com>
2020-11-08 12:45:26 +0500

    Action menu and search components added in Home activity.

--- app/src/main/java/com/bigcompany/ledgerx/SignupActivity.java
+++ app/src/main/java/com/bigcompany/ledgerx/SignupActivity.java
@@ -1,15 +1,22 @@
 package com.bigcompany.ledgerx;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.zip.Inflater;
 
 public class SignupActivity extends AppCompatActivity {
 
     TextView mLoginTxt;
+    Button mSignUpBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +24,18 @@ public class SignupActivity extends AppCompatActivity {
         this.getSupportActionBar().hide();
 
         mLoginTxt = findViewById(R.id.login_textView);
+        mSignUpBtn = findViewById(R.id.signup_button);
 
         mLoginTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(SignupActivity.this, MainActivity.class));
+            }
+        });
+        mSignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignupActivity.this, HomeActivity.class));
             }
         });
     }

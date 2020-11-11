a0264b85817d9b510f18a84648d7bf856824bed4
ranahamza4 <rdxat47@gmail.com>
2020-11-12 00:59:40 +0500

    HomeActivity Recycler Work Done

---
+++ app/src/main/java/com/bigcompany/ledgerx/UserData.java
@@ -0,0 +1,79 @@
+package com.bigcompany.ledgerx;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UserData {
+    private String mUserName;
+    private String mUserID;
+    private String mUserEmail;
+    private int mTotalGiveAmount;
+    private int mTotalTakeAmount;
+  private String mLastTimeEdited;
+    private CircleImageView mUserImage;
+
+
+    public UserData(String mUserName, String mUserID, String mUserEmail, int mTotalGiveAmount, int mTotalTakeAmount) {
+        this.mUserName = mUserName;
+        this.mUserID = mUserID;
+        this.mUserEmail = mUserEmail;
+        this.mTotalGiveAmount = mTotalGiveAmount;
+        this.mTotalTakeAmount = mTotalTakeAmount;
+    }
+
+    public CircleImageView getmUserImage() {
+        return mUserImage;
+    }
+
+    public void setmUserImage(CircleImageView mUserImage) {
+        this.mUserImage = mUserImage;
+    }
+
+    public String getmUserName() {
+        return mUserName;
+    }
+
+    public void setmUserName(String mUserName) {
+        this.mUserName = mUserName;
+    }
+
+    public String getmUserID() {
+        return mUserID;
+    }
+
+    public void setmUserID(String mUserID) {
+        this.mUserID = mUserID;
+    }
+
+    public String getmUserEmail() {
+        return mUserEmail;
+    }
+
+    public void setmUserEmail(String mUserEmail) {
+        this.mUserEmail = mUserEmail;
+    }
+
+    public int getmTotalGiveAmount() {
+        return mTotalGiveAmount;
+    }
+
+    public void setmTotalGiveAmount(int mTotalGiveAmount) {
+        this.mTotalGiveAmount = mTotalGiveAmount;
+    }
+
+    public int getmTotalTakeAmount() {
+        return mTotalTakeAmount;
+    }
+
+    public void setmTotalTakeAmount(int mTotalTakeAmount) {
+        this.mTotalTakeAmount = mTotalTakeAmount;
+    }
+
+
+    public String getmLastTimeEdited() {
+        return mLastTimeEdited;
+    }
+
+    public void setmLastTimeEdited(String mLastTimeEdited) {
+        this.mLastTimeEdited = mLastTimeEdited;
+    }
+}
---
+++ app/src/main/java/com/bigcompany/ledgerx/UserDataAdapter.java
@@ -0,0 +1,75 @@
+package com.bigcompany.ledgerx;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UserDataAdapter extends RecyclerView.Adapter<UserDataAdapter.ViewHolder> {
+
+ private UserData mUserDataArray[];
+ Context context;
+
+    public UserDataAdapter(UserData[] mUserData, HomeActivity activity) {
+        this.mUserDataArray = mUserData;
+        this.context = activity;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view=layoutInflater.inflate(R.layout.data_item_list,parent,false);
+        ViewHolder viewHolder= new ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+ final UserData mUserDataList= mUserDataArray[position];
+
+         holder.mUserNameTextView.setText(mUserDataList.getmUserName());
+//      holder.mLastEditTimeAndDate.setText(mUserDataList.getmLastTimeEdited());
+      holder.mUserGiveTextView.setText(""+mUserDataList.getmTotalGiveAmount());
+      holder.mUserTakeTextView.setText(""+mUserDataList.getmTotalTakeAmount());
+
+      holder.itemView.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              Toast.makeText(context,mUserDataList.getmUserName(),Toast.LENGTH_SHORT).show();
+          }
+      });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUserDataArray.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        private CircleImageView mUserImageView;
+        private TextView mUserNameTextView;
+        private TextView mLastEditTimeAndDate;
+        private TextView mUserGiveTextView;
+        private TextView mUserTakeTextView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+           // mUserGiveTextView= itemView.findViewById(R.id.user_profileimage_view);
+            mUserNameTextView= itemView.findViewById(R.id.other_username_textView);
+          //  mLastEditTimeAndDate=itemView.findViewById(R.id.last_edited_DateAndTime_TextView);
+            mUserGiveTextView=itemView.findViewById(R.id.user_TotalGive_TextView);
+            mUserTakeTextView=itemView.findViewById(R.id.user_TotalTake_TextView);
+
+        }
+    }
+}
